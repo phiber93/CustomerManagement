5e18c562ad29bc8d65c9eba91ac5e5462aae2ce3
phiber93 <153130604+phiber93@users.noreply.github.com>
2024-05-14 09:17:56 +0200

    Initial commit

---
+++ src/main/java/at/phiber93/CustomerManagement/CustomerManagementApplication.java
@@ -0,0 +1,13 @@
+package at.phiber93.CustomerManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementApplication.class, args);
+	}
+
+}
